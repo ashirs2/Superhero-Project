a580f64d1036c21895a93e8fd3d79aa49ce438e4
Ali Shirsalimian <Ali Shirsalimian@DESKTOP-9S97F0T>
2020-10-07 18:17:09 -0500

    Contact Model

---
+++ Superhero-api/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Superhero_api.Models
+{
+    public class Contact
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Nickname { get; set; }
+        public string Place { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+
+        public bool IsDeleted { get; set; } = false;
+
+
+    }
+}
