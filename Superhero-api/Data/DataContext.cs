6f64e78fb6a1af64cd138f037d562fda5bad96fd
Ali Shirsalimian <Ali Shirsalimian@DESKTOP-9S97F0T>
2020-10-14 13:44:39 -0500

    GET,PUT,POST,DELETE

---
+++ Superhero-api/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Superhero_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Superhero_api.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+    }
+}
